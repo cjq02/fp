65443615aa1974910f7c076aa34822688a3fd02c
Jacquinc <Jacquin.Chen@ehealth.com>
2018-03-27 16:12:07 +0800

    Enhance Navigation

--- fp-frontend/src/components/nav.vue
+++ fp-frontend/src/components/nav.vue
@@ -1,6 +1,7 @@
 <template>
   <v-toolbar dark color="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-icon v-if="showHome">home</v-icon>
+    <v-icon v-if="!showHome" @click="goBack">keyboard_arrow_left</v-icon>
     <v-toolbar-title class="white--text">{{title}}</v-toolbar-title>
     <v-spacer></v-spacer>
   </v-toolbar>
@@ -14,21 +15,33 @@ import { muscleItems as items } from "../json/data";
 export default Vue.extend({
   data() {
     return {
-      title: getTitle(this.$route)
+      title: String()
     };
+  },
+  created() {
+    this.title = this.getTitle(this.$route);
+  },
+  computed: {
+    showHome() : boolean {
+      return this.$route.name === 'Home' 
+    }
   },
   watch: {
     $route: function() {
-      this.title = getTitle(this.$route);
+      this.title = this.getTitle(this.$route);
+    }
+  },
+  methods: {
+    getTitle(route: any) {
+      if (route.name === "Record") {
+        let item = _.find(items, { itemId: _.parseInt(route.params.itemId) });
+        return item.itemName;
+      }
+      return route.name;
+    },
+    goBack() {
+      this.$router.go(-1);
     }
   }
 });
-
-let getTitle = (route: any) => {
-  if (route.name === "Record") {
-    let item = _.find(items, {'itemId': _.parseInt(route.params.itemId)})
-    return item.itemName;
-  }
-  return route.name;
-};
 </script>
